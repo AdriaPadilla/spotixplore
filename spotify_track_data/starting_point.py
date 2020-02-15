873101a9148ca5f62f85a48e3976b92e574b8a7c
adriapadilla <adrian.padilla.m@gmail.com>
2020-02-15 10:37:24 +0100

    add more playlists as example on starting_point.py

--- spotify_track_data/starting_point.py
+++ spotify_track_data/starting_point.py
@@ -1,3 +1,3 @@
 # Here you can define a single playlist to extact data, or a list of playlists
 
-PLAYLISTS = ["5zuu72zXv5LxAWmqG2BJXj", "0L9nJRjJiPDFGVQcalB4s9"]
+PLAYLISTS = ["36cMVcjVVofblqXaMByT7t", "0L9nJRjJiPDFGVQcalB4s9", "3oopyXIZGLFtHjFYN9KbuI"]
