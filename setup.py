6e75fe62c0a3156cbf85f574a346427440adb094
adriapadilla <adrian.padilla.m@gmail.com>
2020-02-25 16:21:24 +0100

    new credentials and setup.py version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(name='spotixplore',
-      version="0.1",
+      version="0.1.1",
       url = "https://github.com/AdriaPadilla/spotixplore", 
       description='explore track features and recommended tracks from a playlist',
       author='Adrian Padilla',
       author_email='adrian.padilla.m@gmail.com',
       packages=setuptools.find_packages(),
-      install_requires=["spotipy", "pandas"],
+      install_requires=["spotipy", "pandas", "openpyxl"],
       classifiers=[
        "Programming Language :: Python :: 3",
        "License :: OSI Approved :: MIT License",
