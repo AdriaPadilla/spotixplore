4b6612193cd6cf22b7387a0dfe4eb5a8971f3625
AdriaPaidlla <adrian.padilla.m@gmail.com>
2020-02-28 10:53:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(name='spotixplore',
       version="0.1.3",
       url = "https://github.com/AdriaPadilla/spotixplore", 
-      description='explore track features and recommended tracks from a playlist',
+      description='explore Spotify tracks features and recommended tracks from a playlist',
       author='Adrian Padilla',
       author_email='adrian.padilla.m@gmail.com',
       packages=setuptools.find_packages(),
